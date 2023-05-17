283cea9bf3201d076535a9e2d072c01e42d1f94b
Mateus Guedes <mxteusgued@gmail.com>
2023-05-17 04:15:40 -0300

    finish clouds

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,23 @@
-import { OrbitControls } from "@react-three/drei";
+import { Float, OrbitControls } from "@react-three/drei";
+import Background from "./Background";
+import {AirPlane} from "./AirPlane"
+import { Cloud } from "./Cloud";
+
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Background />
+      <Float floatIntensity={2} speed={2}>
+        <AirPlane rotation-y={Math.PI / 2} scale={[0.2, 0.2, 0.2]} position-y={0.1} />
+      </Float>
+      <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]}/>
+      <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]}/>
+      <Cloud opacity={0.7} scale={[0.3, 0.3, 0.4]} rotation-y={Math.PI / 9} position={[2, -0.2, -2]}/>
+      <Cloud opacity={0.7} scale={[0.4, 0.4, 0.4]} rotation-y={Math.PI / 9} position={[1, -0.2, -12]}/>
+      <Cloud opacity={0.7} scale={[0.5, 0.5, 0.5]} position={[-1, 1, -53]}/>
+      <Cloud opacity={0.3} scale={[0.8, 0.8, 0.8]} position={[0, 1, -100]}/>
     </>
   );
 };
