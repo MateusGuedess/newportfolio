36872049cb70c00b5bd9407afb75dea953d16794
Mateus Guedes <mxteusgued@gmail.com>
2023-05-23 03:59:33 -0300

    added framer motion to do animation

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { usePlay } from '../contexts/Play'
+import { motion, AnimatePresence } from "framer-motion";
 
 
 function Me() {
   const {scrollMoment} = usePlay()
-  // console.log(scrollMoment)
+  //  console.log(scrollMoment)
   return (
-    <div className={`${scrollMoment > 0.1 ? 'show' : ''}`} style={{opacity: 0, position: "absolute", top: "30px", left: "50px"}}>Me</div>
+    <AnimatePresence>
+      {scrollMoment > 0.13 && scrollMoment < 0.15 && <motion.div  
+      className="about-me"
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: scrollMoment * 8, opacity: 1 }}
+      exit={{ opacity: 0, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20
+      }}>Me</motion.div>}
+    </AnimatePresence>
   )
 }
 
