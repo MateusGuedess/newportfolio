daa3a5fca9aeed6369140334120655bc54634c8d
Mateus Guedes <mxteusgued@gmail.com>
2023-05-23 18:52:18 -0300

    added first stop of the travel

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -16,8 +16,16 @@ function Me() {
       transition={{
         type: "spring",
         stiffness: 260,
-        damping: 20
-      }}>Me</motion.div>}
+        damping: 20,
+        delay: 2,
+      }}>
+        <img src="https://placehold.co/200x300" width="200"height="300"/>
+        <div  className="article">
+          <h2>Mateus Guedes da Conceição, 26</h2>
+          Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum girarzis, nisi eros vermeio.
+          Suco de cevadiss deixa as pessoas mais interessantis.Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!
+        </div>
+      </motion.div>}
     </AnimatePresence>
   )
 }
